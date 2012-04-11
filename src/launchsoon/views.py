93d310bfccd46de6d8b481cbeb8d6dccf823c2c4
Abdelkrim Boujraf <abdelkrim.boujraf@gmail.com>
2012-04-11 18:59:00 +0200

    correct bug: button "go" is next to email input box add low resolution back image

--- src/launchsoon/views.py
+++ src/launchsoon/views.py
@@ -62,25 +62,7 @@ class SignUp(webapp.RequestHandler):
         
         path = os.path.join(os.path.dirname(__file__), '..', 'templates', 'thank_you.html')
         self.response.out.write(template.render(path, template_values))
+    
     def get(self):
-        
-        try:
-            i = Interested(email=self.request.get('email'))
-            i.put() 
-            template_values = {
-            'OG_TITLE' : OG_TITLE,
-            'OG_TYPE' : OG_TYPE,
-            'OG_URL' : OG_URL,
-            'OG_IMAGE' : OG_IMAGE,
-            'OG_IMAGE_TYPE' : OG_IMAGE_TYPE,
-            'OG_IMAGE_WIDTH' : OG_IMAGE_WIDTH,
-            'OG_IMAGE_HEIGHT' : OG_IMAGE_HEIGHT,
-            'OG_DESCRIPTION' : OG_DESCRIPTION,
-            'OG_SITE_NAME': OG_SITE_NAME,
-            }
-            path = os.path.join(os.path.dirname(__file__), '..', 'templates', 'thank_you.html')
-            self.response.out.write(template.render(path, template_values))
-            
-        except (TypeError, ValueError):
-            self.response.out.write("<html><body><p>Invalid inputs</p></body></html>")
+        pass
     
