394322801b2f2787361e1441c058853a8d324413
Abdelkrim Boujraf <abdelkrim.boujraf@gmail.com>
2012-04-11 17:58:00 +0200

    split the template index.html into base.html, google_analytics.html, interested.html, navbar.html, opengraph.html, thank_you.html
    When user input an email address and clicks on "go" button, the email address is stored into Interested
    
    Action: reduced the size of the default back.jpg to ±100kb but the size is too small, need to revise this

--- src/launchsoon/models.py
+++ src/launchsoon/models.py
@@ -1,29 +1,24 @@
 '''
 Created on Apr 9, 2012
-
 @author: abdelkrim boujraf
-
-    - Table name 'interested'
-
 '''
 
-class interested(db.Model):
+from google.appengine.ext import db
+
+class Interested(db.Model):
     '''
-    classdocs
+    People who are interested by the experience.
+    We collect their email address and will contact them as soon as the project is ready for a Beta test
+
+USAGE within the Interactive Console: http://localhost:XXXX/_ah/admin/interactive
+
+from google.appengine.ext import db
+
+interested_list = db.GqlQuery("SELECT * FROM Interested")
+
+for interested in interested_list:
+    print interested.email
     '''
 
-
-    def __init__(selfparams):
-        '''
-        Constructor
-        '''
-
-    email = db.StringProperty(indexed=False)
-    joined = db.DateTimeProperty(auto_now_add=True,verbose_name='first time user login',indexed=False)
-            
-    #firstname = db.StringProperty()
-    #lastname = db.StringProperty()
-    #hash = db.StringProperty(indexed=False)
-    #invites
-    #googleID = db.StringProperty(indexed=False)
-    
+    email = db.EmailProperty(required=True)
+    joined = db.DateTimeProperty(auto_now_add=True,verbose_name='first time user login')
--- src/launchsoon/views.py
+++ src/launchsoon/views.py
@@ -11,10 +11,12 @@ from google.appengine.dist import use_library
 use_library('django', '1.3')
 
 from google.appengine.ext import webapp
-from google.appengine.api import users
-import urllib
+
 import os
+
 from settings import *
+from models import Interested
+from google.appengine.ext import db
  
 from google.appengine.ext.webapp import template
 
@@ -42,6 +44,43 @@ class MainPage(webapp.RequestHandler):
 
 class SignUp(webapp.RequestHandler):
     def post(self):
-        pass
+        i = Interested(email=db.Email(self.request.get('email')))
+        i.put() 
+        
+        template_values = {
+            'OG_TITLE' : OG_TITLE,
+            'OG_TYPE' : OG_TYPE,
+            'OG_URL' : OG_URL,
+            'OG_IMAGE' : OG_IMAGE,
+            'OG_IMAGE_TYPE' : OG_IMAGE_TYPE,
+            'OG_IMAGE_WIDTH' : OG_IMAGE_WIDTH,
+            'OG_IMAGE_HEIGHT' : OG_IMAGE_HEIGHT,
+            'OG_DESCRIPTION' : OG_DESCRIPTION,
+            'OG_SITE_NAME': OG_SITE_NAME,
+        }
+        
+        
+        path = os.path.join(os.path.dirname(__file__), '..', 'templates', 'thank_you.html')
+        self.response.out.write(template.render(path, template_values))
     def get(self):
-        pass  
+        
+        try:
+            i = Interested(email=self.request.get('email'))
+            i.put() 
+            template_values = {
+            'OG_TITLE' : OG_TITLE,
+            'OG_TYPE' : OG_TYPE,
+            'OG_URL' : OG_URL,
+            'OG_IMAGE' : OG_IMAGE,
+            'OG_IMAGE_TYPE' : OG_IMAGE_TYPE,
+            'OG_IMAGE_WIDTH' : OG_IMAGE_WIDTH,
+            'OG_IMAGE_HEIGHT' : OG_IMAGE_HEIGHT,
+            'OG_DESCRIPTION' : OG_DESCRIPTION,
+            'OG_SITE_NAME': OG_SITE_NAME,
+            }
+            path = os.path.join(os.path.dirname(__file__), '..', 'templates', 'thank_you.html')
+            self.response.out.write(template.render(path, template_values))
+            
+        except (TypeError, ValueError):
+            self.response.out.write("<html><body><p>Invalid inputs</p></body></html>")
+    
